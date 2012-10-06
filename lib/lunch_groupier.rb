73508806919791798fb6595de2731773317508e4
Your Actual NameDebora <debbieblass@gmail.com>
2012-10-06 16:46:42 +0200

    changed lunch_gropier.rb file in lib

--- lib/lunch_groupier.rb
+++ lib/lunch_groupier.rb
@@ -8,8 +8,35 @@ class LunchGroupier
     @lunch_places = lunch_places
   end
 
+  def create_players_groups
+    num_groups = Player.count/2
+    playersa = Player.find_all_by_department('IT and Product')
+    playersb = Player.find_all_by_department('Customer Support')
+    playersc = Player.find_all_by_department('HR and Administrative')
+    playersd = Player.find_all_by_department('Marketing and Design')
+    playerse = Player.find_all_by_department('Publisher')
+    playersf = Player.find_all_by_department('Advertiser')
+    playersg = Player.find_all_by_department('Billing')
+    
+    player_groups =[]
+    num_groups.times.each do
+      player_groups << []
+    end
+
+    playersall = playersa + playersb + playersc + playersd + playerse + playersf + playersg
+
+    playersall.each_with_index do |p, i|
+      group_num = i % num_groups
+      p "putting player #{p.name} in group #{group_num}"
+      player_groups[group_num]<< p
+    end
+
+
+    player_groups
+  end
+
   def create_groups
-    player_groups = random_groups_of(4, Player.all)
+    player_groups = self.create_players_groups
     names = @group_names.take_random(player_groups.size)
     places = @lunch_places.take_random(player_groups.size)
 
