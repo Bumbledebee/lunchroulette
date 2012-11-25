aa61b97f9ccbe1adb072277a1bd8cb39fc0221e7
Debora <debbieblass@gmail.com>
2012-11-25 12:03:12 +0100

    added pictureuploader and map and I think is ok

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   attr_accessible :comment, :name, :picture
+  mount_uploader :picture, PictureUploader
+
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,6 @@
 class Place < ActiveRecord::Base
   attr_accessible :address, :name
+  geocoded_by :address
+  after_validation :geocode
+  acts_as_gmappable :process_geocoding => false
 end
