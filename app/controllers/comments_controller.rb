ed9b51f550e7afe3ca9ffb5512d9540a399761d5
Debora <debbieblass@gmail.com>
2012-12-05 18:38:10 +0100

    trying to improve

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     @player = Player.find(params[:player_id])
-    @comment = @place.comments.create(params[:comment, :player])
+    @comment = Comments.create(params[:place_id, :player_id]])
     redirect_to place_path(@place)
   end
 end
