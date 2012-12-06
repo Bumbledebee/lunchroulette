265b01e12d34bce7e63bb0a6cfb39f2f2a006a07
Debora <debbieblass@gmail.com>
2012-12-06 22:16:05 +0100

    changed how one passes 2 params in the comment create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     @player = Player.find(params[:player_id])
-    @comment = Comments.create(params[:place_id, :player_id]])
+    @comment = Comments.create(:place_id => params[:place_id], :player_id => params[:player_id])
     redirect_to place_path(@place)
   end
 end
